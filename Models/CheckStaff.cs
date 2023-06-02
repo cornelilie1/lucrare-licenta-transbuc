da21940aabf76e7e9af446416b58f3dffcbc493e
Cornel <cornelilie004@yahoo.com>
2023-06-02 23:03:57 +0300

    Incarcare DRAFT Proiect Licenta

---
+++ Models/CheckStaff.cs
@@ -0,0 +1,14 @@
+﻿namespace TransBuc.Models
+{
+    public class CheckStaff : User  
+    {
+        public bool status { get; set; }
+
+        public CheckStaff(User user, bool status):base(user)
+        {
+            status = status;
+        }
+    }
+
+  
+}
